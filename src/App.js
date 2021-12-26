a17c4a7f852cf6f7a4d2a7e534c145f3c576c81b
Adam L <luzsiadam@gmail.com>
2021-12-26 16:02:16 +0100

    + Introduced react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-//import React from 'react';
+import React from 'react';
 import './App.css';
 
 import './pages/homepage/homepage.styles.scss';
 
+import { Routes, Route, Link } from "react-router-dom";
+
 import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () => (
+   <div>
+      <h1>HATS PAGE</h1>
+   </div>
+ );
 
 function App() {
    return (
       <div>
-         <HomePage />
+         <Link to="/">Home</Link> |{" "}
+         <Link to="/hats">Hats</Link>
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="hats" element={<HatsPage />} />
+         </Routes>
       </div>
    );
 }
