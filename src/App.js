5868b09e12b2744b8c94fd6faa7f6349d9755fb0
Adam L <luzsiadam@gmail.com>
2021-12-26 16:50:17 +0100

    + Experimenting with useNavigate

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 
 import './pages/homepage/homepage.styles.scss';
 
-import { Routes, Route, Link, useParams } from "react-router-dom";
+import { Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
 
 import HomePage from './pages/homepage/homepage.component';
 
 
-const HatsPage = () => (
-   <div>
-      <h1>HATS PAGE</h1>
-   </div>
-);
+const HatsPage = (props) => {
+   console.log(props);
+   return (
+      <div>
+         <h1>HATS PAGE</h1>
+      </div>
+   );
+}
 
 const ProdID = () => {
-   const { pid } = useParams();
+
+   let { pid } = useParams();
+
    return (
       <div>
          <h1>{pid.length < 2 ? "HEHE" : pid}</h1>
@@ -24,14 +29,16 @@ const ProdID = () => {
 };
 
 function App() {
+   let navigate = useNavigate();
    return (
       <div>
          <Link to="/">Home</Link> |{" "}
          <Link to="/hats">Hats</Link> |{" "}
-         <Link to="/prods/jóska">Prod</Link>
+         <Link to="/prods/jóska">Prod</Link> |{" "}
+         <button onClick={() => {navigate("/prods/pista")}}>Pista</button>
          <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="hats" element={<HatsPage />} />
+            <Route path="hats" element={<HatsPage kuki="muki" />} />
             <Route path="prods/*" element={<ProdID />} />
             <Route path="prods/:pid" element={<ProdID />} />
          </Routes>
