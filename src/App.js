3ae5a87d45afb70c8107c6636f548ad68386fdc9
Adam L <luzsiadam@gmail.com>
2021-12-26 16:18:15 +0100

    + Experimenting more with the router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import './pages/homepage/homepage.styles.scss';
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -12,16 +12,28 @@ const HatsPage = () => (
    <div>
       <h1>HATS PAGE</h1>
    </div>
- );
+);
+
+const ProdID = () => {
+   const { pid } = useParams();
+   return (
+      <div>
+         <h1>{pid.length < 2 ? "HEHE" : pid}</h1>
+      </div>
+   )
+};
 
 function App() {
    return (
       <div>
          <Link to="/">Home</Link> |{" "}
-         <Link to="/hats">Hats</Link>
+         <Link to="/hats">Hats</Link> |{" "}
+         <Link to="/prods/jóska">Prod</Link>
          <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="hats" element={<HatsPage />} />
+            <Route path="prods/*" element={<ProdID />} />
+            <Route path="prods/:pid" element={<ProdID />} />
          </Routes>
       </div>
    );
