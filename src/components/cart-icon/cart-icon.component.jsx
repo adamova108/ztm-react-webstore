024d46ec1bd1eaab856509437eef924b57c6532f
Adam L <luzsiadam@gmail.com>
2022-01-02 22:25:18 +0100

    + Added cart icon component

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,38 @@
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import { connect } from "react-redux";
+
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
+import { createStructuredSelector } from "reselect";
+
+import './cart-icon.styles.scss';
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+        <ShoppingIcon className="shopping-icon" />
+        <span className="item-count">{itemCount}</span>
+    </div>
+)
+
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+/* const mapStateToProps = ({ cart: { cartItems }}) => ({
+    itemCount: cartItems.reduce(
+            (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+        )
+}) */
+
+/* const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+}) */
+
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
