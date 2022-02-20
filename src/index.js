e37568d4cf52a31387985ae905cf2bdf48fcff19
Adam L <luzsiadam@gmail.com>
2022-02-20 12:45:38 +0100

    + Updated main index JS

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+
+import { PersistGate } from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store';
+//import persistor from './redux/store';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
